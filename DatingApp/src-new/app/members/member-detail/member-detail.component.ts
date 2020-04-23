901bba5acb35a896c15f38d96aed2caa61f0b5ae
vikas jha <vikasjha405@hotmail.com>
2020-04-24 04:44:14 +0530

    Chnages 1.1

---
+++ DatingApp/src-new/app/members/member-detail/member-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-member-card',
+  templateUrl: './member-detail.component.html',
+  styleUrls: ['./member-detail.component.css']
+})
+export class MemberDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ DatingApp/src-new/app/members/member-messages/MemberMessagesComponent.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-member-card',
+  templateUrl: './MemberMessagesComponent.html',
+  styleUrls: ['./MemberMessagesComponent.css']
+})
+export class MemberMessagesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
